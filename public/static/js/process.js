3512191660f5103d1dd89db1da364fe19cd02080
synfonaut <synfonaut@protonmail.com>
2019-05-05 01:54:32 -0700

    split out settings

--- public/static/js/process.js
+++ public/static/js/process.js
@@ -1,55 +1,9 @@
-const isNode = (typeof window == "undefined");
-const isBrowser = (typeof window == "object");
-
 var axios;
 if (isNode) {
     axios = require("axios");
     markdownit = require("markdown-it");
 }
 
-var SETTINGS = {
-    "api_key": "1D23Q8m3GgPFH15cwseLFZVVGSNg3ypP2z",
-    "api_endpoint": "https://bitomation.com/{api_action}/{api_key}/{query}",
-    "tip_amount": 0.05,
-};
-
-function get_local_settings() {
-    const storage = window.localStorage;
-    const cached_settings = storage["settings"];
-    if (cached_settings) {
-        try {
-            return JSON.parse(cached_settings);
-        } catch (e) {
-        }
-    }
-    return {};
-}
-
-function save_local_settings(local_settings) {
-    if (local_settings) {
-        window.localStorage["settings"] = JSON.stringify(local_settings);
-        console.log("Saving local settings", local_settings);
-    } else {
-        delete window.localStorage["settings"];
-    }
-}
-
-function use_local_settings(local_settings) {
-    if (local_settings && Object.keys(local_settings).length > 0) {
-        console.log("Found local settings", local_settings);
-        for (const key in local_settings) {
-            const val = local_settings[key];
-            console.log("Local setting override", key, val);
-            SETTINGS[key] = val;
-        }
-    }
-}
-
-if (isBrowser) {
-    use_local_settings(get_local_settings());
-}
-
-var BSV_PRICE = 53.00;
 const B_MEDIA_PROTOCOL = "19HxigV4QyBv3tHpQVcUEQyq1pzZVdoAut";
 const BCAT_MEDIA_PROTOCOL = "15DHFxWZJT58f9nhyGnsRBqrgwK4W6h4Up";
 const SUPPORTED_TIPCHAIN_PROTOCOLS = [
@@ -72,7 +26,6 @@ const OPENDIR_ACTIONS = [
     "vote",
     "undo",
 ];
-
 
 function get_bitdb_query(category_id=null, cursor=0, limit=200) {
 
---
+++ public/static/js/settings.js
@@ -0,0 +1,47 @@
+const isNode = (typeof window == "undefined");
+const isBrowser = (typeof window == "object");
+
+var SETTINGS = {
+    "api_key": "1D23Q8m3GgPFH15cwseLFZVVGSNg3ypP2z",
+    "api_endpoint": "https://bitomation.com/{api_action}/{api_key}/{query}",
+    "tip_amount": 0.05,
+};
+
+function get_local_settings() {
+    const storage = window.localStorage;
+    const cached_settings = storage["settings"];
+    if (cached_settings) {
+        try {
+            return JSON.parse(cached_settings);
+        } catch (e) {
+        }
+    }
+    return {};
+}
+
+function save_local_settings(local_settings) {
+    if (local_settings) {
+        window.localStorage["settings"] = JSON.stringify(local_settings);
+        console.log("Saving local settings", local_settings);
+    } else {
+        delete window.localStorage["settings"];
+    }
+}
+
+function use_local_settings(local_settings) {
+    if (local_settings && Object.keys(local_settings).length > 0) {
+        console.log("Found local settings", local_settings);
+        for (const key in local_settings) {
+            const val = local_settings[key];
+            console.log("Local setting override", key, val);
+            SETTINGS[key] = val;
+        }
+    }
+}
+
+var BSV_PRICE = 53.00;
+
+if (isBrowser) {
+    use_local_settings(get_local_settings());
+}
+
