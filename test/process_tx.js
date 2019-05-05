da2c6f1c575b76674beb6580e7f9dbd1ce757158
synfonaut <synfonaut@protonmail.com>
2019-05-04 17:27:43 -0700

    split out process

--- test/process_tx.js
+++ test/process_tx.js
@@ -1,5 +1,5 @@
 var assert = require('assert');
-var helpers = require("../public/static/js/helpers");
+var process = require("../public/static/js/process");
 
 function readFileTXs(filepath) {
     const fs = require('fs');
@@ -17,7 +17,7 @@ describe('basic tx processing', function() {
             {"height":580286,"address":"1Nup3TDg3B1cQj74nRMQRX3VHYY8dTx88A","txid":"2fff57d7f40b31e55448468b6aec45ffaddf34278aca8de1098ee9adcf560f18","data":{"s1":"1dirxA5oET8EmcdW4saKXzPqejmMXQwg2","s2":"category.create","s3":"name","s4":"New directory","s5":"description","s6":"This is a new directory"}}
         ];
 
-        const processedResults = helpers.processResults(txs);
+        const processedResults = process.processResults(txs);
         assert.equal(processedResults[0].type, "category");
         assert.equal(processedResults[0].name, "New directory");
         assert.equal(processedResults[0].description, "This is a new directory");
@@ -37,7 +37,7 @@ describe('basic tx processing', function() {
             {"height":580286,"address":"1Juvf2KaCJearuF1zFRxrnRCocmjdh3DcC","txid":"a8b337d22a91f6a2c4263ee2e3bfae6bc88a91af5ce80d217c6b9cf1a0a534a3","data":{"s1":"1dirxA5oET8EmcdW4saKXzPqejmMXQwg2","s2":"entry.update","s3":"f126fbdd09832f446505604ea82842b6cc3da76b261b9f264d07da9e5fab671d","s4":"name","s5":"New link with edit","s6":"description","s7":"Desc goes here (edit)"}},
             {"height":580286,"address":"1Juvf2KaCJearuF1zFRxrnRCocmjdh3DcC","txid":"a8b337d22a91f6a2c4263ee2e3bfae6bc88a91af5ce80d217c6b9cf1a0a534a4","data":{"s1":"1dirxA5oET8EmcdW4saKXzPqejmMXQwg2","s2":"entry.update","s3":"f126fbdd09832f446505604ea82842b6cc3da76b261b9f264d07da9e5fab671d","s4":"name","s5":"Newer link with edit","s6":"description","s7":"Desc goes here (edited)"}},
         ];
-        const processedResults = helpers.processResults(txs);
+        const processedResults = process.processResults(txs);
         assert.equal(processedResults[0].type, "entry");
         assert.equal(processedResults[0].name, "Newer link with edit");
         assert.equal(processedResults[0].link, "bit://asdf");
@@ -60,7 +60,7 @@ describe('process open directory undo transactions', function() {
             {"name":"New link","link":"bit://asdf","description":"Desc goes here","rendered_description":"Desc goes here","category":"2fff57d7f40b31e55448468b6aec45ffaddf34278aca8de1098ee9adcf560f18","type":"entry","txid":"f126fbdd09832f446505604ea82842b6cc3da76b261b9f264d07da9e5fab671d","address":"1DcVxjZ56dqYTPejKanoUXfrzypSei2fNp","height":580286,"votes":0}
         ];
 
-        const processedResults = helpers.processResults(txs);
+        const processedResults = process.processResults(txs);
 
         assert.equal(processedResults[0].type, "entry");
         assert.equal(processedResults[0].name, "New link");
@@ -78,7 +78,7 @@ describe('process open directory undo transactions', function() {
             {"height":null,"address":"1LLzzUcjT6Gb2eW24cXSTYnQjw3EhA6Cds","txid":"7b5cc3cd1a7ddd3aae32b7e21c623c99027ff885af68a7fd5ae47062fc18dce9","data":{"s1":"1dirxA5oET8EmcdW4saKXzPqejmMXQwg2","s2":"undo","s3":"8b5cc3cd1a7ddd3aae32b7e21c623c99027ff885af68a7fd5ae47062fc18dce8"}}
         ];
 
-        const processedResults = helpers.processResults(txs);
+        const processedResults = process.processResults(txs);
         assert.equal(processedResults[0].type, "entry");
         assert.equal(processedResults[0].name, "New link with edit");
         assert.equal(processedResults[0].link, "bit://asdf");
@@ -101,7 +101,7 @@ describe('process open directory undo transactions', function() {
             {"name":"New link","link":"bit://asdf","description":"Desc goes here","rendered_description":"Desc goes here","category":"2fff57d7f40b31e55448468b6aec45ffaddf34278aca8de1098ee9adcf560f18","type":"entry","txid":"f126fbdd09832f446505604ea82842b6cc3da76b261b9f264d07da9e5fab671d","address":"1DcVxjZ56dqYTPejKanoUXfrzypSei2fNp","height":580286,"votes":0}
         ];
 
-        const processedResults = helpers.processResults(txs);
+        const processedResults = process.processResults(txs);
 
         assert.equal(processedResults[0].type, "entry");
         assert.equal(processedResults[0].name, "New link");
@@ -123,7 +123,7 @@ describe('process open directory undo transactions', function() {
             {"name":"New directory","description":"This is a new directory","rendered_description":"This is a new directory","type":"category","txid":"2fff57d7f40b31e55448468b6aec45ffaddf34278aca8de1098ee9adcf560f18","address":"1Nup3TDg3B1cQj74nRMQRX3VHYY8dTx88A","height":580286,"votes":0,"entries":0},
         ];
 
-        const processedResults = helpers.processResults(txs);
+        const processedResults = process.processResults(txs);
         assert.equal(processedResults[0].type, "category");
         assert.equal(processedResults[0].name, "New directory");
         assert.equal(processedResults[0].description, "This is a new directory");
@@ -132,7 +132,7 @@ describe('process open directory undo transactions', function() {
 
     it('converts 4x undo regression', function() {
         const txs = readFileTXs("undo_undo_undo_undo_entry_regression.json");
-        const processedResults = helpers.processResults(txs);
+        const processedResults = process.processResults(txs);
 
         assert.equal(processedResults.length, 2);
 
@@ -158,8 +158,8 @@ describe('tipchain', function() {
             {"height":580286,"address":"1Nup3TDg3B1cQj74nRMQRX3VHYY8dTx88A","txid":"2fff57d7f40b31e55448468b6aec45ffaddf34278aca8de1098ee9adcf560f18","data":{"s1":"1dirxA5oET8EmcdW4saKXzPqejmMXQwg2","s2":"category.create","s3":"name","s4":"New directory","s5":"description","s6":"This is a new directory"}}
         ];
 
-        const processedResults = helpers.processResults(txs);
-        assert.equal(processedResults[0].tipchain[0].address, helpers.OPENDIR_TIP_ADDRESS);
+        const processedResults = process.processResults(txs);
+        assert.equal(processedResults[0].tipchain[0].address, process.OPENDIR_TIP_ADDRESS);
         assert.equal(processedResults[0].tipchain[1].address, "1Nup3TDg3B1cQj74nRMQRX3VHYY8dTx88A");
         assert.equal(processedResults[0].txid, "2fff57d7f40b31e55448468b6aec45ffaddf34278aca8de1098ee9adcf560f18");
     });
@@ -172,8 +172,8 @@ describe('tipchain', function() {
             {"height":580286,"address":"1Juvf2KaCJearuF1zFRxrnRCocmjdh3DcC","txid":"a8b337d22a91f6a2c4263ee2e3bfae6bc88a91af5ce80d217c6b9cf1a0a534a3","data":{"s1":"1dirxA5oET8EmcdW4saKXzPqejmMXQwg2","s2":"entry.update","s3":"f126fbdd09832f446505604ea82842b6cc3da76b261b9f264d07da9e5fab671d","s4":"name","s5":"New link with edit","s6":"description","s7":"Desc goes here (edit)"}}, // currently tipchain ignores edits
         ];
 
-        const processedResults = helpers.processResults(txs);
-        assert.equal(processedResults[1].tipchain[0].address, helpers.OPENDIR_TIP_ADDRESS);
+        const processedResults = process.processResults(txs);
+        assert.equal(processedResults[1].tipchain[0].address, process.OPENDIR_TIP_ADDRESS);
         assert.equal(processedResults[1].tipchain[1].address, "1Nup3TDg3B1cQj74nRMQRX3VHYY8dTx88A");
         assert.equal(processedResults[1].tipchain[2].address, "1DcVxjZ56dqYTPejKanoUXfrzypSei2fNp");
         assert.equal(processedResults[1].txid, "f126fbdd09832f446505604ea82842b6cc3da76b261b9f264d07da9e5fab671d");
@@ -187,8 +187,8 @@ describe('tipchain', function() {
 
         ];
 
-        const processedResults = helpers.processResults(txs);
-        assert.equal(processedResults[2].tipchain[0].address, helpers.OPENDIR_TIP_ADDRESS);
+        const processedResults = process.processResults(txs);
+        assert.equal(processedResults[2].tipchain[0].address, process.OPENDIR_TIP_ADDRESS);
         assert.equal(processedResults[2].tipchain[1].address, "1Jtp4DDg3B1cQj74nRMQRX3VHYY8dTx99B");
         assert.equal(processedResults[2].tipchain[2].address, "1Nup3TDg3B1cQj74nRMQRX3VHYY8dTx88A");
         assert.equal(processedResults[2].tipchain[3].address, "1DcVxjZ56dqYTPejKanoUXfrzypSei2fNp");
@@ -199,16 +199,16 @@ describe('tipchain', function() {
 
 describe("tipchain split", function() {
     it('tipchain split for one address', function() {
-        const tipchain = [helpers.OPENDIR_TIP_ADDRESS];
-        const splits = helpers.calculateTipchainSplits(tipchain);
+        const tipchain = [process.OPENDIR_TIP_ADDRESS];
+        const splits = process.calculateTipchainSplits(tipchain);
         assert.equal(tipchain.length, splits.length);
         assert.equal(splits[0], 1);
         assert(splits.reduce((a, b) => { return a+b }) == 1);
     });
 
     it('tipchain split for two addresses', function() {
-        const tipchain = [helpers.OPENDIR_TIP_ADDRESS, "1Jtp4DDg3B1cQj74nRMQRX3VHYY8dTx99B"];
-        const splits = helpers.calculateTipchainSplits(tipchain);
+        const tipchain = [process.OPENDIR_TIP_ADDRESS, "1Jtp4DDg3B1cQj74nRMQRX3VHYY8dTx99B"];
+        const splits = process.calculateTipchainSplits(tipchain);
         assert.equal(tipchain.length, splits.length);
         assert(Math.abs(splits[0] - 0.371) < FLOAT_TOLERANCE);
         assert(Math.abs(splits[1] - 0.629) < FLOAT_TOLERANCE);
@@ -216,8 +216,8 @@ describe("tipchain split", function() {
     });
 
     it('tipchain split for three addresses', function() {
-        const tipchain = [helpers.OPENDIR_TIP_ADDRESS, "1Jtp4DDg3B1cQj74nRMQRX3VHYY8dTx99B", "1Nup3TDg3B1cQj74nRMQRX3VHYY8dTx88A"];
-        const splits = helpers.calculateTipchainSplits(tipchain);
+        const tipchain = [process.OPENDIR_TIP_ADDRESS, "1Jtp4DDg3B1cQj74nRMQRX3VHYY8dTx99B", "1Nup3TDg3B1cQj74nRMQRX3VHYY8dTx88A"];
+        const splits = process.calculateTipchainSplits(tipchain);
         assert.equal(tipchain.length, splits.length);
         assert(Math.abs(splits[0] - 0.209) < FLOAT_TOLERANCE);
         assert(Math.abs(splits[1] - 0.353) < FLOAT_TOLERANCE);
@@ -226,8 +226,8 @@ describe("tipchain split", function() {
     });
 
     it('tipchain split for four addresses', function() {
-        const tipchain = [helpers.OPENDIR_TIP_ADDRESS, "1Jtp4DDg3B1cQj74nRMQRX3VHYY8dTx99B", "1Nup3TDg3B1cQj74nRMQRX3VHYY8dTx88A", "1DcVxjZ56dqYTPejKanoUXfrzypSei2fNp"];
-        const splits = helpers.calculateTipchainSplits(tipchain);
+        const tipchain = [process.OPENDIR_TIP_ADDRESS, "1Jtp4DDg3B1cQj74nRMQRX3VHYY8dTx99B", "1Nup3TDg3B1cQj74nRMQRX3VHYY8dTx88A", "1DcVxjZ56dqYTPejKanoUXfrzypSei2fNp"];
+        const splits = process.calculateTipchainSplits(tipchain);
         assert.equal(tipchain.length, splits.length);
         assert(Math.abs(splits[0] - 0.139) < FLOAT_TOLERANCE);
         assert(Math.abs(splits[1] - 0.235) < FLOAT_TOLERANCE);
@@ -237,15 +237,15 @@ describe("tipchain split", function() {
     });
 
     it('tipchain payment for one address', function() {
-        const tipchain = [helpers.OPENDIR_TIP_ADDRESS];
-        const payments = helpers.calculateTipPayment(tipchain, 50000, "BSV");
+        const tipchain = [process.OPENDIR_TIP_ADDRESS];
+        const payments = process.calculateTipPayment(tipchain, 50000, "BSV");
         assert.deepEqual(payments, [ { address: "1LPe8CGxypahVkoBbYyoHMUAHuPb4S2JKL", value: 50000, currency: "BSV" } ]);
         assert.equal(payments.map(p => { return p.value }).reduce((a, b) => { return a+b }), 50000);
     });
 
     it('tipchain payment for two address', function() {
-        const tipchain = [helpers.OPENDIR_TIP_ADDRESS, "1Jtp4DDg3B1cQj74nRMQRX3VHYY8dTx99B"];
-        const payments = helpers.calculateTipPayment(tipchain, 50000, "BSV");
+        const tipchain = [process.OPENDIR_TIP_ADDRESS, "1Jtp4DDg3B1cQj74nRMQRX3VHYY8dTx99B"];
+        const payments = process.calculateTipPayment(tipchain, 50000, "BSV");
         assert.deepEqual(payments, [
             { address: '1LPe8CGxypahVkoBbYyoHMUAHuPb4S2JKL', value: 18565.6396, currency: 'BSV' },
             { address: "1Jtp4DDg3B1cQj74nRMQRX3VHYY8dTx99B", value: 31434.3604, currency: "BSV" },
@@ -255,8 +255,8 @@ describe("tipchain split", function() {
     });
 
     it('tipchain payment for three address', function() {
-        const tipchain = [helpers.OPENDIR_TIP_ADDRESS, "1Jtp4DDg3B1cQj74nRMQRX3VHYY8dTx99B", "1Nup3TDg3B1cQj74nRMQRX3VHYY8dTx88A"];
-        const payments = helpers.calculateTipPayment(tipchain, 50000, "BSV");
+        const tipchain = [process.OPENDIR_TIP_ADDRESS, "1Jtp4DDg3B1cQj74nRMQRX3VHYY8dTx99B", "1Nup3TDg3B1cQj74nRMQRX3VHYY8dTx88A"];
+        const payments = process.calculateTipPayment(tipchain, 50000, "BSV");
         assert.deepEqual(payments, [
             { address: '1LPe8CGxypahVkoBbYyoHMUAHuPb4S2JKL', value: 10434.5805, currency: 'BSV' },
             { address: '1Jtp4DDg3B1cQj74nRMQRX3VHYY8dTx99B', value: 17667.2806, currency: 'BSV' },
@@ -267,8 +267,8 @@ describe("tipchain split", function() {
     });
 
     it('tipchain payment for four address', function() {
-        const tipchain = [helpers.OPENDIR_TIP_ADDRESS, "1Jtp4DDg3B1cQj74nRMQRX3VHYY8dTx99B", "1Nup3TDg3B1cQj74nRMQRX3VHYY8dTx88A", "1DcVxjZ56dqYTPejKanoUXfrzypSei2fNp"];
-        const payments = helpers.calculateTipPayment(tipchain, 50000, "BSV");
+        const tipchain = [process.OPENDIR_TIP_ADDRESS, "1Jtp4DDg3B1cQj74nRMQRX3VHYY8dTx99B", "1Nup3TDg3B1cQj74nRMQRX3VHYY8dTx88A", "1DcVxjZ56dqYTPejKanoUXfrzypSei2fNp"];
+        const payments = process.calculateTipPayment(tipchain, 50000, "BSV");
         assert.deepEqual(payments, [
             { address: "1LPe8CGxypahVkoBbYyoHMUAHuPb4S2JKL", value: 6965.6764, currency: "BSV" },
             { address: "1Jtp4DDg3B1cQj74nRMQRX3VHYY8dTx99B", value: 11793.9153, currency: "BSV" },
@@ -280,16 +280,16 @@ describe("tipchain split", function() {
     });
 
     it('tipchain works for very large chains', function() {
-        const tipchain = Array(15).fill(helpers.OPENDIR_TIP_ADDRESS);
-        const payments = helpers.calculateTipPayment(tipchain, 50000, "BSV");
+        const tipchain = Array(15).fill(process.OPENDIR_TIP_ADDRESS);
+        const payments = process.calculateTipPayment(tipchain, 50000, "BSV");
         assert.equal(tipchain.length, payments.length);
         const total = payments.map(p => { return p.value }).reduce((a, b) => { return a+b });
         assert(Math.abs(total - 50000) < FLOAT_TOLERANCE);
     });
 
     it('tipchain payment for two address (usd)', function() {
-        const tipchain = [helpers.OPENDIR_TIP_ADDRESS, "1Jtp4DDg3B1cQj74nRMQRX3VHYY8dTx99B"];
-        const payments = helpers.calculateTipPayment(tipchain, 1, "USD");
+        const tipchain = [process.OPENDIR_TIP_ADDRESS, "1Jtp4DDg3B1cQj74nRMQRX3VHYY8dTx99B"];
+        const payments = process.calculateTipPayment(tipchain, 1, "USD");
         assert.deepEqual(payments, [
             { address: "1LPe8CGxypahVkoBbYyoHMUAHuPb4S2JKL", value: 0.3713, currency: "USD" },
             { address: "1Jtp4DDg3B1cQj74nRMQRX3VHYY8dTx99B", value: 0.6287, currency: "USD" },
@@ -299,8 +299,8 @@ describe("tipchain split", function() {
     });
 
     it('large tipchain with small payment (usd)', function() {
-        const tipchain = Array(15).fill(helpers.OPENDIR_TIP_ADDRESS);
-        const payments = helpers.calculateTipPayment(tipchain, 0.05, "USD");
+        const tipchain = Array(15).fill(process.OPENDIR_TIP_ADDRESS);
+        const payments = process.calculateTipPayment(tipchain, 0.05, "USD");
         assert.deepEqual(payments, [
             { address: '1LPe8CGxypahVkoBbYyoHMUAHuPb4S2JKL', value: 0.0012, currency: 'USD' },
             { address: '1LPe8CGxypahVkoBbYyoHMUAHuPb4S2JKL', value: 0.002, currency: 'USD' },
@@ -323,8 +323,8 @@ describe("tipchain split", function() {
     });
 
     it('tipchain removes empty outputs', function() {
-        const tipchain = [helpers.OPENDIR_TIP_ADDRESS, "1Jtp4DDg3B1cQj74nRMQRX3VHYY8dTx99B"];
-        const payments = helpers.calculateTipPayment(tipchain, 0.0001, "USD");
+        const tipchain = [process.OPENDIR_TIP_ADDRESS, "1Jtp4DDg3B1cQj74nRMQRX3VHYY8dTx99B"];
+        const payments = process.calculateTipPayment(tipchain, 0.0001, "USD");
         assert.deepEqual(payments, [
             { address: "1Jtp4DDg3B1cQj74nRMQRX3VHYY8dTx99B", value: 0.0001, currency: "USD" },
         ]);
@@ -334,13 +334,13 @@ describe("tipchain split", function() {
 
     it('tipchain payment for four address and complex input', function() {
         const tipchain = [
-            {"address": helpers.OPENDIR_TIP_ADDRESS},
+            {"address": process.OPENDIR_TIP_ADDRESS},
             {"address": "1Jtp4DDg3B1cQj74nRMQRX3VHYY8dTx99B"},
             {"address": "1Nup3TDg3B1cQj74nRMQRX3VHYY8dTx88A"},
             {"address": "1DcVxjZ56dqYTPejKanoUXfrzypSei2fNp"}
         ];
 
-        const payments = helpers.calculateTipPayment(tipchain, 50000, "BSV");
+        const payments = process.calculateTipPayment(tipchain, 50000, "BSV");
         assert.deepEqual(payments, [
             { address: "1LPe8CGxypahVkoBbYyoHMUAHuPb4S2JKL", value: 6965.6764, currency: "BSV" },
             { address: "1Jtp4DDg3B1cQj74nRMQRX3VHYY8dTx99B", value: 11793.9153, currency: "BSV" },
